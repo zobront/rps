d867131822fdced885cf87351ce23ae140cfc746
Zach Obront <zobront@gmail.com>
2021-11-05 14:16:01 -0500

    first commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,5 +2,25 @@
 pragma solidity ^0.8.4;
 
 contract Contract {
-	
+	mapping (address => uint) public balances;
+
+    event Result(uint indexed result, uint indexed amount, uint indexed newBalance);
+
+    constructor() {
+        balances[msg.sender] = 1000;
+    }
+
+    function bet(uint amount, uint selection) public {
+        require(selection < 3, "Invalid selection");
+        require(amount < balances[msg.sender], "Not enough funds");
+        if (selection == 2) {
+            balances[msg.sender] += amount;
+            emit(2, amount, balances[msg.sender]);
+        } else if (selection == 1) {
+            emit(1, amount, balances[msg.sender]);
+        } else {
+            balances[msg.sender] -= amount;
+            emit(0, amount, balances[msg.sender]);
+        }
+    }
 }
